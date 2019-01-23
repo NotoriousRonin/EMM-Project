fb65f75c389c73886b36256069f38a5a82759ea7
Tony Nguyen <Nguyen.Tony@gmx.de>
2019-01-23 21:51:53 +0100

    added Audio to Doorbell and Alarm
    Source:
    https://www.youtube.com/watch?v=PowGPSdAxTI
    
    http://soundbible.com/1466-Doorbell.html

---
+++ Interactive House/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource audioSourceAlarm;
+    public AudioSource audioSourceKlingel;
+
+    private SerialCommunicator serialCommunicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialCommunicator = GetComponent<SerialCommunicator>();
+        audioSourceAlarm.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        activateSound(serialCommunicator.klingelState, audioSourceKlingel);
+        activateSound(serialCommunicator.isAlarm, audioSourceAlarm);
+        deactivateSound(serialCommunicator.isAlarm, audioSourceAlarm);
+    }
+
+    private void deactivateSound(bool activate, AudioSource audio)
+    {
+        if (!activate && audio.isPlaying)
+        {
+            audio.Stop();
+        }       
+    }
+
+    private void activateSound(bool activate, AudioSource audio)
+    {
+        if (activate && activate != audio.isPlaying)
+        {
+            audio.Play();
+        }
+    }
+}
