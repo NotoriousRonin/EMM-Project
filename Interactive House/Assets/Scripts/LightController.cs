c2de2c9b9b01de9581b74283d8d5ed6b3ba338d5
Tony Nguyen <Nguyen.Tony@gmx.de>
2019-01-23 20:24:39 +0100

    added Light Controller

---
+++ Interactive House/Assets/Scripts/LightController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Light lightsource;
+
+    private SerialCommunicator serialCommunicator;
+
+    private int baseLumen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialCommunicator = GetComponent<SerialCommunicator>();
+        baseLumen = serialCommunicator.lichtsensorState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float percentage = ((float)serialCommunicator.lichtsensorState / (float)baseLumen);
+        if (percentage > 3) percentage = 3;
+        if (percentage < 0) percentage = 0;
+        lightsource.intensity = Mathf.Round(percentage * 10) / 10;
+    }
+}
---
+++ Interactive House/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WeatherController : MonoBehaviour
+{
+
+    public GameObject weather;
+    //Rain1,Clouds1,Fog
+    ParticleSystem[] particleSystems;
+    bool weatherActive=false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        particleSystems = weather.GetComponentsInChildren<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        checkWeatherActive();
+    }
+
+    //Schnittstelle nach außen (LeapMotion)
+    public void setActivateWeather(bool b)
+    {
+        if (b == true)
+        {
+            weatherActive=true;
+            manipulateEmissions(24,200,10);
+        }
+        else
+        {
+            weatherActive = false;
+            manipulateEmissions(0,0,0);
+        }
+    }
+
+    private void manipulateEmissions(float clouds, float rain, float fog)
+    {
+        var emission = particleSystems[0].emission;
+        emission.rateOverTime = clouds;
+        emission = particleSystems[1].emission;
+        emission.rateOverTime = rain;
+        emission = particleSystems[2].emission;
+        emission.rateOverTime = fog;
+    }
+
+    private void checkWeatherActive()
+    {
+        if (weatherActive == true)
+        {
+            weather.SetActive(true);
+        }
+
+        if (weatherActive == false && particleSystems[0].particleCount==0)
+        {
+            weather.SetActive(false);
+        }
+
+    }
+
+    
+
+}
