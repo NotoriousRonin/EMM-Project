19f6226e84725524be2c6e6301a8a320a92ac2dc
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2019-01-24 19:33:31 +0100

    Weather collision, AlarmLed wechselt Farbe & Magnet/Kamin-Anmation impl

---
+++ Interactive House/Assets/Scripts/AlarmMaterial.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmMaterial : MonoBehaviour
+{
+    private SerialCommunicator SerialCommunicator;
+
+    public Material matRed;
+    public Material matNormal;
+    public Light light;
+    private float t = 0;
+
+
+    void Start()
+    {
+        SerialCommunicator = GameObject.Find("Controller").GetComponent<SerialCommunicator>();
+
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (SerialCommunicator.isAlarm == true)
+        {
+            changeBetweenMaterials();
+        }
+        else
+        {
+            changeMaterial();
+        }
+
+
+
+    }
+
+    private void changeBetweenMaterials()
+    {
+        Renderer  renderer= this.gameObject.GetComponent<Renderer>();
+        renderer.material = matRed;
+        light.intensity = 10;
+
+        //WaitForSeconds(0.25)??
+        /*
+        renderer.material = matNormal;
+        light.intensity = 0;
+        */
+    }
+
+
+    private void changeMaterial()
+    {
+        Renderer renderer = this.gameObject.GetComponent<Renderer>();
+        renderer.material = matNormal;
+        light.intensity = 0;
+    }
+
+}
---
+++ Interactive House/Assets/Scripts/Magnet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour {
+
+    private SerialCommunicator SerialCommunicator;
+    public Animator animatorHouse;
+
+    void Start()
+    {
+        SerialCommunicator = GameObject.Find("Controller").GetComponent<SerialCommunicator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (SerialCommunicator.magnetState == true)
+        {
+            animatorHouse.SetBool("magnetState", true);
+        }
+        else
+        {
+            animatorHouse.SetBool("magnetState", false);
+        }
+
+    }
+
+}
